7e43f08f57da10e1bad5d56f93b0f1a238bfde9a
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2020-10-08 12:39:40 +0200

    updated from content of previous workshop

--- src/controllers/recipe-controller.php
+++ src/controllers/recipe-controller.php
@@ -4,15 +4,32 @@ require __DIR__ . '/../models/recipe-model.php';
 
 function browseRecipes(): void
 {
+    // Fetching all recipes
     $recipes = getAllRecipes();
 
+    // Generate the web page
     require __DIR__ . '/../views/index.php';
 }
 
 function showRecipe(int $id): void
 {
+    // Input parameter validation (integer >0)
+    $id = filter_var($id, FILTER_VALIDATE_INT, ["options" => ["min_range" => 1]]);
+    if (false === $id || null === $id) {
+        header("Location: /");
+        exit("Wrong input parameter");
+    }
+
+    // Fetching a recipe
     $recipe = getRecipeById($id);
 
+    // Result check
+    if (!isset($recipe['title']) || !isset($recipe['description'])) {
+        header("Location: /");
+        exit("Recipe not found");
+    }
+
+    // Generate the web page
     require __DIR__ . '/../views/show.php';
 }
 
@@ -20,14 +37,18 @@ function addRecipe(): void
 {
     if ($_SERVER["REQUEST_METHOD"] === 'POST') {
         $recipe = array_map('trim', $_POST);
+
+        // Validate data
         $errors = validateRecipe($recipe);
 
+        // Save the recipe
         if (empty($errors)) {
             saveRecipe($recipe);
             header('Location: /');
         }
     }
 
+    // Generate the web page
     require __DIR__ . '/../views/form.php';
 }
 
